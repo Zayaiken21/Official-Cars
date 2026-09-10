db35ebe7d80c91054df6e3e508dc53087beb44ca
Zayaiken21 <zayaiken2001@gmail.com>
2026-09-10 04:32:24 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.OFFICIAL_CARS_CONFIG={API_BASE:'https://official-cars-v2.onrender.com',DATA_VERSION:'10'};
+window.OFFICIAL_CARS_CONFIG={API_BASE:'https://official-cars-v2.onrender.com',DATA_VERSION:'11'};
